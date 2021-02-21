db09eee88264ac69effa0072277bc3930c555263
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-02-21 16:41:23 +0530

    Create valid username regular expression.java

---
+++ valid username regular expression.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+     //String pattern = "^[a-zA-Z]\\w{7,29}$";
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+}
+
