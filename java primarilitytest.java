20176fff4a4417a0431062547828f4b8f73ec377
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-02-22 12:00:58 +0530

    Create java primarilitytest.java

---
+++ java primarilitytest.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+
+        scanner.close();
+        Boolean b;
+        BigInteger b1;
+        b1=new BigInteger(n);
+        b=b1.isProbablePrime(1);
+        if(b==true)
+            System.out.println("prime");
+        else
+            System.out.println("not prime");
+    }
+}
